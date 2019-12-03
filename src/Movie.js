928eddf6724a5a9cf702d2979c5c7bcbeb60c002
zedonora <timeundtime@gmail.com>
2019-12-04 07:36:58 +0900

    not modified

---
+++ src/Movie.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie({id, year, title, summary, poster}) {
+    return <h4>{title}</h4>;
+}
+Movie.prototypes = {
+    id : PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+}
+export default Movie;
